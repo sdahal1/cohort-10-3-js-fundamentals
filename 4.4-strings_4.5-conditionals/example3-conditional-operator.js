bbfef3ad2184f0b2d7b01472c32e0caef42d1614
= <=>
2022-10-04 11:24:12 -0700

    day 2 pm complete

---
+++ 4.4-strings_4.5-conditionals/example3-conditional-operator.js
@@ -0,0 +1,10 @@
+let num = 1;
+
+//if num is == 23, log "this is jordan" else log "who dis"
+
+num===23
+    ? console.log("This is Jordan") 
+    : num === 24
+        ? console.log("Kobe!!")
+        :console.log("who dis");
+
