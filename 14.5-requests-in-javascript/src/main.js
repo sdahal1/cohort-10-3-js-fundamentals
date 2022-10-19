b69b3ee470803c545cf5a6ad459edf5e4ab2f5e3
Saurabh <raabdahl@gmail.com>
2022-10-19 12:12:01 -0700

    wednesday pm

--- 14.5-requests-in-javascript/src/main.js
+++ 14.5-requests-in-javascript/src/main.js
@@ -3,62 +3,123 @@ const BASE_URL = "http://localhost:5000";
 
 
 function findAllPlayers(){
+  let result = axios.get(`${BASE_URL}/players`) //make a refund request and ups says they will be arriving eventually (didnt tell me exactly how long it will take) -> make a api call and we will get back some data form the api, but
+
+  console.log("brush my teeth")
+  result.then((responseFromApi)=>{//result.then is going to run only when the api data (result), fully finishes resolving
+    console.log("ups driver arrived! Api returned info", responseFromApi.data)
+    console.log(responseFromApi.status)
+
+  })
+  .catch(err=>{
+    console.log("errrrrorrr, something went wrong!", err.message)
+  })
+
+  console.log("learn javascript")
+  console.log("result looks like this->", result)
 
 }
+
+// findAllPlayers()
 
 function findPlayerById(id){
   axios.get(`${BASE_URL}/players/${id}`)
+    .then(({data})=>{
+      console.log(data)
+    })
+    // .then((response)=>{
+    //   const {data} = response
+    //   console.log(data)
+    // })
+    .catch(err=>{
+      console.log(err.message)
+    })
+}
+
+// findPlayerById(3)
+
+
+function createPlayer(body){
+  axios.post(`${BASE_URL}/players`, body)
     .then(response=>{
       console.log(response.data)
-     
     })
-    .catch()
+    .catch(err=>{
+      console.log(err.message)
+    })
 }
+
+let body = {
+  name: "Klay",
+  team: "Warriors",
+  number_championships: 4,
+  state: "California"
+}
+// createPlayer(body)
 
 function updatePlayerById(id){
-  let newData = {
-    "name": "Goat Bron"
+  let body = {
+    id: "2",
+    name: "Chef Curry",
+    team: "Warriors",
+    number_championships: 4,
+    state: "California"
   }
 
-  axios.put(`${BASE_URL}/players/${id}`, newData)
-    .then(response=>{
-      console.log(response.data)
+  axios.put(`${BASE_URL}/players/${id}`,body)
+    .then(({data})=>{
+      console.log(data)
     })
-    .catch()
+    .catch(err=>{
+      console.log(err.message)
+    })
 }
 
+
+// updatePlayerById(2)
 
 function deletePlayerById(id){
- 
   axios.delete(`${BASE_URL}/players/${id}`)
-    .then(response=>{
-      console.log(response.data)
+  .then(({data})=>{
+    console.log(data)
+  })
+    .catch(err=>{
+      console.log(err.message)
     })
-    .catch()
+ 
 }
 
+// deletePlayerById(8)
+
 function getAllPlayerNames() {
-  axios.get(`${BASE_URL}/players`)
+  return axios.get(`${BASE_URL}/players`)
     .then(response=>{
-      let result = response.data.map(playerObj=>{
+      //console.log(response.data) //response.data is an array of player objects!
+      let playersArray = response.data;
+      let playersNamesOnly = playersArray.map((playerObj)=>{
         return playerObj.name
       })
-      console.log(result)
+      console.log(playersNamesOnly)
+      return playersNamesOnly
     })
 }
+
+// console.log(getAllPlayerNames())
 
 function getPlayersByState(stateName) {
   axios.get(`${BASE_URL}/players`)
     .then(response=>{
-      let playersFromCity = response.data.filter(playerObj=>{
+      let playersArray = response.data;
+      let playersFromState = playersArray.filter(playerObj=>{
         return playerObj.state === stateName
       })
 
-      console.log(playersFromCity)
-      return playersFromCity
+      console.log(playersFromState)
+      return playersFromState
     })
-    .catch()
 }
+
+// getPlayersByState("California")
 
 
 module.exports = {
